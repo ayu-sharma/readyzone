31df882a49930508fbd8df308fb58b26a0b1a30d
AkshatJha21 <akshatj2134@gmail.com>
2023-02-08 19:21:19 +0530

    cart css updated

--- src/common/Cart/Cart.jsx
+++ src/common/Cart/Cart.jsx
@@ -10,12 +10,9 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
     <>
       <section className='cart-items'>
         <div className='container d_flex'>
-          {/* if hamro cart ma kunai pani item xaina bhane no diplay */}
 
           <div className='cart-details'>
-            {CartItem.length === 0 && <h1 className='no-items product'>No Items are add in Cart</h1>}
-
-            {/* yasma hami le cart item lai display garaaxa */}
+            {CartItem.length === 0 && <h1 className='no-items product font-medium'>No Items are add in Cart</h1>}
             {CartItem.map((item) => {
               const productQty = item.price * item.qty
 
@@ -27,8 +24,8 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
                   <div className='cart-details'>
                     <h3>{item.name}</h3>
                     <h4>
-                      ${item.price}.00 * {item.qty}
-                      <span>${productQty}.00</span>
+                      Rs.{item.price} x {item.qty}
+                      <span>Rs.{productQty}</span>
                     </h4>
                   </div>
                   <div className='cart-items-function'>
@@ -37,15 +34,12 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
                         <i className='fa-solid fa-xmark'></i>
                       </button>
                     </div>
-                    {/* stpe: 5 
-                    product ko qty lai inc ra des garne
-                    */}
                     <div className='cartControl d_flex'>
-                      <button className='incCart' onClick={() => addToCart(item)}>
+                      <button className='incCart bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br' onClick={() => addToCart(item)}>
                         <i className='fa-solid fa-plus'></i>
                       </button>
-                      <button className='desCart' onClick={() => decreaseQty(item)}>
-                        <i className='fa-solid fa-minus'></i>
+                      <button className='desCart bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br' onClick={() => decreaseQty(item)}>
+                        <i className='fa-solid fa-minus text-white'></i>
                       </button>
                     </div>
                   </div>
@@ -55,14 +49,14 @@ const Cart = ({ CartItem, addToCart, decreaseQty }) => {
               )
             })}
           </div>
-
-          <div className='cart-total product'>
-            <h2>Cart Summary</h2>
-            <div className=' d_flex'>
-              <h4>Total Price :</h4>
-              <h3>${totalPrice}.00</h3>
+            <div className='w-[20rem] bg-white px-4 py-6 rounded-lg mb-auto'>
+              <h2 className="text-lg text-[#8f0fff] mb-5 pb-2.5 border-b-[rgb(3_0_71_/_9%)] border-b border-solid">Cart Summary</h2>
+              <div className='d_flex my-5'>
+                <h4 className="text-[15px] font-normal">Total Price </h4>
+                <h3 className="text-xl font-medium text-[#8f0fff]">Rs.{totalPrice}</h3>
+              </div>
+              <button className="text-white text-center w-full flow-root float-right py-2 px-4 bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br rounded-md">Proceed To Pay</button>
             </div>
-          </div>
         </div>
       </section>
     </>
